a53826124b7be43f3db82688fcb8469d85484e43
achintyakaushaljha <achintya.kaushaljha@infra.market>
2024-10-28 21:32:29 +0530

    implementing logging functionality in framework by creating logger file using Logging Filters

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -2,7 +2,7 @@ package cucumber.Options;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features",glue = {"stepDefinitions"})
+@RunWith(Cucumber.class)//The runner class allows you to use Cucumber with a test framework like JUnit or TestNG, which provides structure and integration with build tools (like Maven or Gradle). For example:
+@CucumberOptions(features = "src/test/java/features",glue = {"stepDefinitions"})//glue specifies package where cucumber should look for stepdef
 public class TestRunner {
 }
--- src/test/java/resources/utils.java
+++ src/test/java/resources/utils.java
@@ -1,14 +1,21 @@
 package resources;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 import static io.restassured.RestAssured.baseURI;
 
 public class utils {
-    public RequestSpecification requestSpecBuilder(){
-        RequestSpecification req=new RequestSpecBuilder().setBaseUri(baseURI).setContentType(ContentType.JSON).build();
+    public RequestSpecification requestSpecBuilder() throws FileNotFoundException {
+        PrintStream stream = new PrintStream(new FileOutputStream("logger.txt"));//Stream object of print stream class directs all the log generated to logger.txt file created at run time using Fileoutputstream object
+        RequestSpecification req=new RequestSpecBuilder().setBaseUri(baseURI).setContentType(ContentType.JSON).addFilter(RequestLoggingFilter.logRequestTo(stream)).addFilter(ResponseLoggingFilter.logResponseTo(stream)).build();
         return req;
     }
 }
